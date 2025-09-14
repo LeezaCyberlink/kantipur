372d98aec002b87435b0117b5acf0dc4ad772dea
Leeza@218 <leezashakya218@gmail.com>
2025-09-14 10:17:38 +0545

    first commit

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/uikit.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/global.css">
+        <script src="https://kit.fontawesome.com/7254a5967d.js" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+    <!-- navbar section start-->
+     <nav>
+        Header
+     </nav>
+    <!-- navbar section end-->
+    
